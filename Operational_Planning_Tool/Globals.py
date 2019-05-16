4e91276201540e37a49e68ed037d6391ea59f58d
sckanberg <sckanberg@gmail.com>
2019-05-16 13:36:53 +0200

    Removed old public packages and modules

--- Operational_Planning_Tool/Globals.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Globals for Operational_Planning_Tool
-
-current_pointing: Contains the pointing altitude of the last scheduled TC_acfLimbPointingAltitudeOffset command
-
-"""
-
-current_pointing = None
-science_mode_timeline_path = None
