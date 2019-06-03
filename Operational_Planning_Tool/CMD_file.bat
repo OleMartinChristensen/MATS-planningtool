482c1509e662cafa84f1663f85b9216ec0bcab96
sckanberg <sckanberg@gmail.com>
2019-06-03 10:47:54 +0200

    Create CMD_file.bat

---
+++ Operational_Planning_Tool/CMD_file.bat
@@ -0,0 +1,15 @@
+conda install -c anaconda ephem
+
+conda install -c conda-forge matplotlib 
+
+conda install -c anaconda scipy 
+
+conda install -c anaconda lxml
+
+conda install -c astropy astroquery
+
+conda install pip
+
+pip install pymap3d
+
+conda install spyder
