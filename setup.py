acb72fc6a3def6d7fee2f01c52f908d302850026
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2020-09-10 10:07:09 +0200

    added setup.py for installation

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+from setuptools import setup
+
+setup(
+    name="Operational_Planning_Tool",
+    version="1.0",
+    description="Mission planning tool for the MATS satellite",
+    author="David Skånberg & Ole Martin Christensen",
+    author_email="ole.martin.christensen@gmail.com",
+    url="https://github.com/OleMartinChristensen/MATS-planningtool/",
+    packages=["OPT"],
+    install_requires=[
+        "ephem==3.7.6.0",
+        "matplotlib>=3.1.1",
+        "scipy>=1.2.1",
+        "lxml>=4.3.3",
+        "astropy>=4.0",
+        "h5py>=2.9.0",
+        "skyfield>=1.13",
+        "Sphinx>=2.3.1",
+    ],
+)
+
