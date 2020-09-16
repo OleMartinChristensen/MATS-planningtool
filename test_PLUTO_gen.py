e8c103fb1d0b6037e6da8997c839fe821655657b
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2020-09-16 11:44:23 +0200

    added output directory to .gitignore changed names of output files

--- test_PLUTO_gen.py
+++ test_PLUTO_gen.py
@@ -12,25 +12,25 @@ def test_read_xml():
 
 
 def test_write_header():
-    pluto.PLUTOGenerator.write_header("test_header.plp")
+    pluto.PLUTOGenerator.write_header("Output/test_header.plp")
     return
 
 
 def test_write_footer():
-    pluto.PLUTOGenerator.write_header("test_footer.plp")
-    pluto.PLUTOGenerator.write_footer("test_footer.plp")
+    pluto.PLUTOGenerator.write_header("Output/test_footer.plp")
+    pluto.PLUTOGenerator.write_footer("Output/test_footer.plp")
     return
 
 
 def test_write_all():
     pluto.PLUTOGenerator.PLUTO_generator(
-        "test_data/XML_TIMELINE__MinimalScience_.xml", "minimal_science.plp"
+        "test_data/XML_TIMELINE__MinimalScience_.xml", "Output/minimal_science.plp"
     )
     return
 
 
 def test_write_mimnimal_script():
     pluto.PLUTOGenerator.PLUTO_generator(
-        "test_data/XML_TIMELINE__MinimalPluto_.xml", "minimal_pluto.plp"
+        "test_data/XML_TIMELINE__MinimalPluto_.xml", "Output/minimal_pluto.plp"
     )
     return
